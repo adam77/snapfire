b94d025157138f48df8d335aa772f376bd34d3e0
adam77 <adam77@65e60406-da2b-11de-b50f-7b9694e7f7ce>
2010-08-22 23:57:50 +0000

    more data file updates

--- war/CHAOS_legion.js
+++ war/CHAOS_legion.js
@@ -1,50 +1,46 @@
-// OPTIONALS
-var raptors = 		{id:10, label:'Raptors', upto:8, minimum:4};
-	var raptor = 	{id:11, label:'Raptor', pts:35, optional:true, group:raptors};
-	raptors.options = [raptor];
-var terms = 		{id:12, label:'Terminators', upto:6, minimum:4};
-	var term = 		{id:13, label:'Chaos Terminator', pts:65, optional:true, group:terms};
-	terms.options = [term];
-var lords = 		{id:14, label:'Lord', upto:1, minimum:1};
-	var lord = 		{id:15, label:'Chaos Lord', pts:0, optional:true, group:lords};
-	var sorcerer =	{id:16, label:'Sorcerer Lord', pts:0, optional:true, group:lords};
-	var prince =	{id:17, label:'Daemon Prince', pts:50, optional:true, group:lords};
-	var warlord =	{id:18, label:'Chaos Warlord', pts:0, optional:true, group:lords};
-	lords.options = [warlord, lord, sorcerer, prince];
-var armour =		{id:19, label:'Vehicles', upto:8, minimum:4};
-	var pred =		{id:20, label:'Chaos Predator', pts:50, optional:true, group:armour};
-	var raider2 =	{id:21, label:'Chaos Land Raider', pts:75, optional:true, group:armour};
-	armour.options = [pred, raider2];
-var assault =		{id:22, label:'Vehicles', upto:3, minimum:1};
-	var decimator2 ={id:23, label:'Decimator', pts:225, optional:true, group:assault};
-	var wheel =		{id:24, label:'Death Wheel', pts:275, optional:true, group:assault};
-	assault.options = [decimator2, wheel];
+// MANDATORY UNITS
+var raptors = 		{id:11, label:'Raptor', pts:35, optional:true, upto:8, minimum:4};
+var terms =			{id:13, label:'Chaos Terminator', pts:65, optional:true, upto:6, minimum:4};
+var lords = 		{id:14, label:'Lord', upto:1, minimum:1, options:[
+	{id:15, label:'Chaos Lord', pts:0, optional:true},
+	{id:16, label:'Sorcerer Lord', pts:0, optional:true},
+	{id:17, label:'Daemon Prince', pts:50, optional:true},
+	{id:18, label:'Chaos Warlord', pts:0, optional:true}
+]};
+var armour =		{id:19, label:'Vehicles', upto:8, minimum:4, options:[
+	{id:20, label:'Chaos Predator', pts:50, optional:true},
+	{id:21, label:'Chaos Land Raider', pts:75, optional:true}
+]};
+var assault =		{id:22, label:'Vehicles', upto:3, minimum:1, options:[
+	{id:23, label:'Decimator', pts:225, optional:true},
+	{id:24, label:'Death Wheel', pts:275, optional:true}
+]};
 
 // UPGRADES
-var pact = 				{id:40, label:'Daemonic Pact', upto:1, pts:25};
-var dread = 			{id:41, label:'Dreadnoughts', upto:3, pts:50};
-var defiler = 			{id:42, label:'Defiler', upto:3, pts:75};
-var raiders =			{id:43, label:'Chaos Land Raider', upto:4, pts:75};
-var claws =				{id:44, label:'Dreadclaw Drop Pod', pts:5};
-var obliterator =		{id:45, label:'Obliterator', upto:3, pts:75};
-var champion =			{id:46, label:'Chaos Champion', upto:1, pts:50};
-var icon =				{id:47, label:'Icon Bearer', upto:1, pts:50};
-var rhino =				{id:48, label:'Rhino', pts:10};
-var havoc =				{id:49, label:'4 Havocs', upto:1, pts:150};
-var cultmarine =		{id:50, label:'4 Cult Marines', upto:1, pts:150};
-var greater =			{id:51, label:'Greater Daemon', pts:100};
-var lesser =			{id:52, label:'Lesser Daemon', pts:20};
+var pact = 			{id:40, label:'Daemonic Pact', upto:1, pts:25};
+var dread = 		{id:41, label:'Dreadnoughts', upto:3, pts:50};
+var defiler = 		{id:42, label:'Defiler', upto:3, pts:75};
+var raiders =		{id:43, label:'Chaos Land Raider', upto:4, pts:75};
+var claws =			{id:44, label:'Dreadclaw Drop Pod', pts:5};
+var obliterator =	{id:45, label:'Obliterator', upto:3, pts:75};
+var champion =		{id:46, label:'Chaos Champion', upto:1, pts:50};
+var icon =			{id:47, label:'Icon Bearer', upto:1, pts:50};
+var rhino =			{id:48, label:'Rhino', pts:10};
+var havoc =			{id:49, label:'4 Havocs', upto:1, pts:150};
+var cultmarine =	{id:50, label:'4 Cult Marines', upto:1, pts:150};
+var greater =		{id:51, label:'Greater Daemon', pts:100};
+var lesser =		{id:52, label:'Lesser Daemon', pts:20};
 
 // DETACHMENTS
 var detachmentList =	{id:500, label:'DETACHMENTS', options:[
-	{id:501, label:'Armoured Company',			pts:0, displayPts:200, defaults:[{unit:pred, count:4}], upgrades:[pred, raider2, defiler]},
-	{id:502, label:'Assault Company',			pts:0, displayPts:225, upto:2, defaults:[{unit:decimator2, count:1}], upgrades:[decimator2, wheel, defiler]},
-	{id:503, label:'Bike Company',				pts:300, units:'8 Chaos Space Marines Bikes', defaults:[{unit:lord, count:1}], upgrades:[icon, pact]},
-	{id:504, label:'Black Legion Retinue',		pts:275, units:'8 Chaos Space Marines', defaults:[{unit:lord, count:1}], upgrades:[rhino, havoc, cultmarine, dread, defiler, raiders, claws, obliterator, pact, champion, icon]},
-	{id:505, label:'Chaos Terminators',			pts:0, upto:2, displayPts:260, defaults:[{unit:lord, count:1}, {unit:term,count:4}], upgrades:[term, dread, defiler, raiders, claws, obliterator, pact, champion, icon]},
+	{id:501, label:'Armoured Company',			pts:0, displayPts:200, upgrades:[armour,defiler]},
+	{id:502, label:'Assault Company',			pts:0, displayPts:225, upto:2, upgrades:[assault,defiler]},
+	{id:503, label:'Bike Company',				pts:300, units:'8 Chaos Space Marines Bikes', upgrades:[lords,icon,pact]},
+	{id:504, label:'Black Legion Retinue',		pts:275, units:'8 Chaos Space Marines', upgrades:[lords,rhino,havoc, cultmarine, dread, defiler, raiders, claws, obliterator, pact, champion, icon]},
+	{id:505, label:'Chaos Terminators',			pts:0, upto:2, displayPts:260, upgrades:[lords,terms,dread, defiler, raiders, claws, obliterator, pact, champion, icon]},
 	{id:506, label:'Daemon Pool',				pts:0, upto:1, upgrades:[greater, lesser]},
-	{id:507, label:'Forlorn Hope Retinue',		pts:125, units:'4 Chaos Space Marines', upto:1, upgrades:[rhino, dread, pact]},
-	{id:508, label:'Raptor Cult', 				pts:0, upto:1, displayPts:140, defaults:[{unit:lord, count:1}, {unit:raptor,count:4}], upgrades:[raptor, pact]}
+	{id:507, label:'Forlorn Hope Retinue',		pts:125, units:'4 Chaos Space Marines', upto:1, upgrades:[rhino,dread,pact]},
+	{id:508, label:'Raptor Cult', 				pts:0, upto:1, displayPts:140, upgrades:[lords,raptors,pact]}
 ]};
 
 // NAVY
--- war/EL_dark_NETEA.js
+++ war/EL_dark_NETEA.js
@@ -1,51 +1,43 @@
-// OPTIONALS
-var escorts = 	{id:10, label:'Escorts', minimum:1, upto:3};
-var escort =	{id:11, label:'Corsair Class Escort',	pts:150,	optional:true,	group:escorts};
-escorts.options =[escort];
-var ravens = 	{id:12, label:'Fighters', minimum:2, upto:3};
-var raven =		{id:13, label:'Raven Fighter',			pts:100,	optional:true,	group:ravens};
-ravens.options =[raven];
-var razors = 	{id:14, label:'Bombers', minimum:2, upto:3};
-var razor =		{id:15, label:'Razorwing Bomber',		pts:125,	optional:true,	group:razors};
-razors.options =[razor];
-var dracons2 =	{id:16, label:'Dracon', minimum:1, upto:1};
-var dracon2	 =	{id:17, label:'Dracon', 				pts:50, 	optional:true, group:dracons2};
-var archon2 =	{id:18, label:'Archon', 				pts:100,	optional:true, group:dracons2};
-dracons2.options=[dracon2,archon2];
-var ravagers =	{id:19,	label:'Ravagers', minimum:1, upto:1};
-var ravager4 =	{id:20,	label:'4 Ravagers',				pts:0,		optional:true, group:ravagers};
-var ravager6 =	{id:21,	label:'6 Ravagers',				pts:100,	optional:true, group:ravagers};
-ravagers.options=[ravager4,ravager6];
-var barges =	{id:22,	label:'Barges',	minimum:1,	upto:2};
-var barge =		{id:23,	label:'Vessel of Pain',			pts:250,	optional:true, group:barges};
-barges.options=[barge];
+// MANDATORY UNITS
+var escort =	{id:11, label:'Corsair Class Escort',	pts:150,	optional:true,	minimum:1, upto:3};
+var raven =		{id:13, label:'Raven Fighter',			pts:100,	optional:true,	minimum:2, upto:3};
+var razor =		{id:15, label:'Razorwing Bomber',		pts:125,	optional:true,	minimum:2, upto:3};
+var barge =		{id:23,	label:'Vessel of Pain',			pts:250,	optional:true, 	minimum:1, upto:2};
+var dracons2 =	{id:16, label:'Dracon', minimum:1, upto:1, options:[
+	{id:17, label:'Dracon', 				pts:50, 	optional:true},
+	{id:18, label:'Archon', 				pts:100,	optional:true}
+]};
+var ravagers =	{id:19,	label:'Ravagers', minimum:1, upto:1, options:[
+	{id:20,	label:'4 Ravagers',				pts:0,		optional:true},
+	{id:21,	label:'6 Ravagers',				pts:100,	optional:true}
+]};
 
 // UPGRADES
-var dracons =		{id:50, label:'Dracon', upto:1};
-var dracon	 =		{id:51, label:'Dracon', 			pts:50, 	group:dracons};
-var archon =		{id:52, label:'Archon', 			pts:100,	group:dracons};
-dracons.options = 	[dracon, archon];
+var dracons =		{id:50, label:'Dracon', upto:1, options:[
+	{id:51, label:'Dracon', 			pts:50},
+	{id:52, label:'Archon', 			pts:100}
+]};
 var sybarite =		{id:53, label:'Sybarite', 			pts:25,		upto:1};
-var upgrades2 =		{id:54, label:'Infantry Upgrades',	upto:2};
-var incubi2 =		{id:55, label:'2 Incubi',			pts:125,	group:upgrades2};
-var wych2 =			{id:56, label:'2 Wyches',			pts:100,	group:upgrades2};
-var warrior2 =		{id:57, label:'2 Warriors',			pts:100,	group:upgrades2};
-var mandrake2 =		{id:58, label:'2 Mandrakes',		pts:125,	group:upgrades2};
-var scourge2 =		{id:59, label:'2 Scourges',			pts:125,	group:upgrades2};
-var haemonculi2 =	{id:60, label:'Haemonculus, Grotesque',	pts:150,	group:upgrades2};
-upgrades2.options = [incubi2,wych2,warrior2,mandrake2,scourge2,haemonculi2];
+var upgrades2 =		{id:54, label:'Infantry Upgrades',	upto:2, options:[
+	{id:55, label:'2 Incubi',			pts:125},
+	{id:56, label:'2 Wyches',			pts:100},
+	{id:57, label:'2 Warriors',			pts:100},
+	{id:58, label:'2 Mandrakes',		pts:125},
+	{id:59, label:'2 Scourges',			pts:125},
+	{id:60, label:'Haemonculus, Grotesque',	pts:150}
+]};
 var ravager =		{id:61, label:'Ravager',			pts:50,		upto:1};
-var upgrades =		{id:62, label:'Infantry Upgrade',	upto:1};
-var incubi =		{id:63, label:'2 Incubi',			pts:125,	group:upgrades};
-var wych =			{id:64, label:'2 Wyches',			pts:100,	group:upgrades};
-var warrior =		{id:65, label:'2 Warriors',			pts:100,	group:upgrades};
-var mandrake =		{id:66, label:'2 Mandrakes',		pts:125,	group:upgrades};
-var scourge =		{id:67, label:'2 Scourges',			pts:125,	group:upgrades};
-var haemonculi =	{id:68, label:'Haemonculus, Grotesque',	pts:150,group:upgrades};
-var beasts =		{id:75,	label:'2 Warp Beasts',		pts:100,	group:upgrades};
-var hellion =		{id:76,	label:'2 Hellions',			pts:100,	group:upgrades};
-var reaver =		{id:77,	label:'2 Reavers',			pts:100,	group:upgrades};
-upgrades.options = [incubi,wych,warrior,mandrake,scourge,haemonculi,beasts];
+var upgrades =		{id:62, label:'Infantry Upgrade',	upto:1, options:[
+	{id:63, label:'2 Incubi',			pts:125},
+	{id:64, label:'2 Wyches',			pts:100},
+	{id:65, label:'2 Warriors',			pts:100},
+	{id:66, label:'2 Mandrakes',		pts:125},
+	{id:67, label:'2 Scourges',			pts:125},
+	{id:68, label:'Haemonculus, Grotesque',	pts:150},
+	{id:75,	label:'2 Warp Beasts',		pts:100},
+	{id:76,	label:'2 Hellions',			pts:100},
+	{id:77,	label:'2 Reavers',			pts:100}
+]};
 var pain =			{id:69, label:'Vessel of Pain',		pts:250,	upto:1};
 var raider =		{id:70, label:'Raider',				pts:0,		upto:4};
 var pleasure =		{id:71, label:'Barge of Pleasure',	pts:125,	upto:2};
@@ -53,17 +45,17 @@ var slave =			{id:72,	label:'Slavebringer',		pts:200,	upto:1};
 var talos =			{id:73,	label:'Talos',				pts:50,		upto:2};
 var perditor =		{id:74,	label:'Perditor',			pts:150,	upto:2};
 
-// PARTISANS (2 per KABAL) - todo:replace with defaults and optionals where appropriate
+// PARTISANS (2 per KABAL) - todo:fix infantry upgrades
 var troupeList = {id:500, label:'PARTISANS', options:[
-	{id:501, label:'Haemonculi Coven',			pts:250, 		units:'2 Haemonculi, 4 Grotesques', upgrades:[haemonculi,talos,perditor,sybarite,pain,raider,slave], restricted:true},
+	{id:501, label:'Haemonculi Coven',			pts:250, 		units:'2 Haemonculi, 4 Grotesques', upgrades:[talos,perditor,sybarite,pain,raider,slave], restricted:true},
 	{id:502, label:'Talos Swarm',				pts:200, 		units:'4 Talos', upgrades:[talos,perditor,slave], restricted:true},
-	{id:503, label:'Wych Cult',					pts:200, 		units:'6 Wyches', upgrades:[wych,beasts,hellion,reaver,sybarite,raider,pleasure,slave], restricted:true},
-	{id:504, label:'Hellion Murder',			pts:200, 		units:'6 Hellions', upgrades:[hellion,sybarite], restricted:true},
-	{id:505, label:'Reaver Gang',				pts:200, 		units:'6 Reavers', 	upgrades:[reaver,sybarite], restricted:true},
-	{id:506, label:'Mandrake Throng',			pts:225, 		units:'6 Mandrakes', upgrades:[mandrake], restricted:true},
-	{id:507, label:'Scourge Flight',			pts:225, 		units:'6 Scourges', upgrades:[scourge,sybarite], restricted:true},
-	{id:508, label:'Heavy Barges',				pts:0, displayPts:250,	defaults:[{unit:barge,count:1}], upgrades:[barge], restricted:true}
-]}
+	{id:503, label:'Wych Cult',					pts:200, 		units:'6 Wyches', upgrades:[upgrades,sybarite,raider,pleasure,slave], restricted:true},
+	{id:504, label:'Hellion Murder',			pts:200, 		units:'6 Hellions', upgrades:[upgrades,sybarite], restricted:true},
+	{id:505, label:'Reaver Gang',				pts:200, 		units:'6 Reavers', 	upgrades:[upgrades,sybarite], restricted:true},
+	{id:506, label:'Mandrake Throng',			pts:225, 		units:'6 Mandrakes', upgrades:[upgrades], restricted:true},
+	{id:507, label:'Scourge Flight',			pts:225, 		units:'6 Scourges', upgrades:[upgrades,sybarite], restricted:true},
+	{id:508, label:'Heavy Barges',				pts:0, displayPts:250,	upgrades:[barge], restricted:true}
+]};
 
 // INDIVIDUALS
 var individualList = {id:532, label:'INDIVIDUALS', options:[
@@ -73,20 +65,20 @@ var individualList = {id:532, label:'INDIVIDUALS', options:[
 
 // KABALS
 var warhostList =	{id:510, label:'KABALS', options:[
-	{id:511, label:'Kabal Coterie', 	pts:200, displayPts:250, units:'4 Incubi',	defaults:[{unit:dracon2, count:1}], restricting:true,	upgrades:[sybarite,upgrades2,ravager,raider,pleasure,slave], upto:1},
-	{id:512, label:'Kabal Syndicate',	pts:200, restricting:true,	upgrades:[dracons,sybarite,incubi,wych,warrior,mandrake,scourge,haemonculi,ravager,raider,pleasure,slave], units:'6 Warriors'},
-	{id:513, label:'Kabal Flotilla',	pts:250, restricting:true,	defaults:[{unit:ravager4,count:1}], upgrades:[dracons,sybarite,pain]}
+	{id:511, label:'Kabal Coterie', 	pts:200, displayPts:250, units:'4 Incubi', restricting:true,	upgrades:[dracons2,sybarite,upgrades2,ravager,raider,pleasure,slave], upto:1},
+	{id:512, label:'Kabal Syndicate',	pts:200, restricting:true,	upgrades:[dracons,sybarite,upgrades2,ravager,raider,pleasure,slave], units:'6 Warriors'},
+	{id:513, label:'Kabal Flotilla',	pts:250, restricting:true,	upgrades:[dracons,sybarite,ravagers,pain]}
 ]};
 
 // ALLIES
 var spacecraft = 	{id:520, label:'Spacecraft', upto:1};
 var allyList = 		{id:521, label:'SPACESHIPS, AIRCRAFT &amp; HORRORS', options:[
 	{id:522, label:'Torture Class Cruiser',		pts:300, upgrades:[], limited:true, group:spacecraft},
-	{id:523, label:'Corsair Escorts',			pts:0, displayPts:150, defaults:[{unit:escort,count:1}], upgrades:[escort], limited:true, group:spacecraft},
+	{id:523, label:'Corsair Escorts',			pts:0, displayPts:150, upgrades:[escort], limited:true, group:spacecraft},
 	{id:524, label:'Tormentor Titan',			pts:500, upgrades:[], limited:true},
 	{id:525, label:'Executioner Landing Module',pts:750, upgrades:[], limited:true},
-	{id:526, label:'Fighter Formation',			pts:0, displayPts:200, defaults:[{unit:raven,count:2}], upgrades:[raven], limited:true},
-	{id:527, label:'Bomber Formation',			pts:0, displayPts:250, defaults:[{unit:razor,count:2}], upgrades:[razor], limited:true}
+	{id:526, label:'Fighter Formation',			pts:0, displayPts:200, upgrades:[raven], limited:true},
+	{id:527, label:'Bomber Formation',			pts:0, displayPts:250, upgrades:[razor], limited:true}
 ]};
 
 // DETAILS
