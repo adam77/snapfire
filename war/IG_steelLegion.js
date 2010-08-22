30cf5a4c4a11a7151b4937064fe9a1dce1143a10
adam77 <adam77@65e60406-da2b-11de-b50f-7b9694e7f7ce>
2010-08-22 07:26:01 +0000

    more data file updates

--- war/IG_steelLegion.js
+++ war/IG_steelLegion.js
@@ -1,74 +1,78 @@
-// UNIT OPTIONS
-var lemanRuss =			{id:10, label:'Leman Russ',	upto:10,	minimum:10};
-	var russ =				{id:11, label:'Leman Russ',	pts:0,	group:lemanRuss,	optional:true};
-	var vanquisher =		{id:12, label:'Vanquisher',	pts:0,	group:lemanRuss,	optional:true, upto:1};
-	lemanRuss.options =		[russ, vanquisher];
-var superh =			{id:13, label:'Super Heavys',	upto:3,		minimum:3};
-	var bane =				{id:14, label:'Baneblade',		pts:0,	group:superh,	optional:true};
-	var sword =				{id:15, label:'Shadowsword',	pts:0,	group:superh,	optional:true};
-	superh.options =		[bane, sword];
-var superP =			{id:16, label:'Super Heavy',	upto:1,		minimum:1};
-	var bane2 =				{id:17, label:'Baneblade',		pts:0,	group:superP,	optional:true};
-	var sword2 =			{id:18, label:'Shadowsword',	pts:0,	group:superP,	optional:true};
-	superP.options =		[bane2, sword2];
-var artilleryOpts =		{id:19, label:'Artillery',		upto:9,		minimum:9};
-	var basil =				{id:20, label:'Basilisk',		pts:0,	group:artilleryOpts,optional:true};
-	var manti =				{id:21, label:'Manticore',		pts:0,	group:artilleryOpts,optional:true};
-	artilleryOpts.options =	[basil, manti];
-var spacecraft = 		{id:22, label:'Spacecraft', 	upto:1,		minimum:1};
-	var lunar =				{id:23, label:'Lunar Class',	pts:0,	group:spacecraft,	optional:true};
-	var emperor	=			{id:24, label:'Emperor Class',	pts:150,group:spacecraft,	optional:true};
-	spacecraft.options =	[lunar, emperor];
+// MANDATORY UNITS
+var lemanRuss =			{id:10, label:'Leman Russ',		upto:10,	minimum:10, options:[
+	{id:11, label:'Leman Russ',	pts:0,	optional:true},
+	{id:12, label:'Vanquisher',	pts:0,	optional:true, upto:1}
+]};
+var superh =			{id:13, label:'Super Heavys',	upto:3,		minimum:3, options:[
+	{id:14, label:'Baneblade',		pts:0,	optional:true},
+	{id:15, label:'Shadowsword',	pts:0,	optional:true}
+]};
+var superP =			{id:16, label:'Super Heavy',	upto:1,		minimum:1, options:[
+	{id:17, label:'Baneblade',		pts:0,	optional:true},
+	{id:18, label:'Shadowsword',	pts:0,	optional:true}
+]};
+var artilleryOpts =		{id:19, label:'Artillery',		upto:9,		minimum:9, options:[
+	{id:20, label:'Basilisk',		pts:0,	optional:true},
+	{id:21, label:'Manticore',		pts:0,	optional:true}
+]};
+var spacecraft = 		{id:22, label:'Spacecraft', 	upto:1,		minimum:1, options:[
+	{id:23, label:'Lunar Class',	pts:0,	optional:true},
+	{id:24, label:'Emperor Class',	pts:150,optional:true}
+]};
 
 // UPGRADES (upto 3 per company)
-var companyUpgrades = 	{id:25, label:'Upgrades', upto:3};
-	var chimera =			{id:26, label:'Chimera',							pts:25}; // only applies to snipers & ogryns
-	var fireSupport =		{id:27, label:'Fire Support Platoon (4 Fire Support)',	pts:100, upto:1, group:companyUpgrades};
-	var infantry =			{id:28, label:'Infantry Platoon (6 Infantry)',		pts:100, upto:1, group:companyUpgrades};
-	var tankSquadron =		{id:29, label:'Tank Squadron (3 Leman Russ)',		pts:200, upto:1, group:companyUpgrades};
-	var demolisher =		{id:30, label:'Tank Squadron (3 Demolishers)',		pts:200, upto:1, group:companyUpgrades};
-	var hellhounds =		{id:31, label:'Hellhound Squadron (3 Hellhounds)',	pts:150, upto:1, group:companyUpgrades};
-	var griffons =			{id:32, label:'Griffon Battery (3 Griffons)',		pts:100, upto:1, group:companyUpgrades};
-	var snipers =			{id:33, label:'2 Snipers',							pts:50, upto:1, group:companyUpgrades};
-	var ogryns =			{id:34, label:'2 Ogryns',							pts:50, upto:1, group:companyUpgrades};
-	var hydra =				{id:35, label:'Hydra',								pts:50, upto:1, group:companyUpgrades};
-	companyUpgrades.options = [fireSupport, infantry, tankSquadron, demolisher, hellhounds, griffons, snipers, ogryns, hydra];
+var companyUpgrades = 	{id:25, label:'Upgrades', upto:3, options:[
+	{id:27, label:'Fire Support Platoon (4 Fire Support)',	pts:100, upto:1},
+	{id:28, label:'Infantry Platoon (6 Infantry)',		pts:100, upto:1},
+	{id:29, label:'Tank Squadron (3 Leman Russ)',		pts:200, upto:1},
+	{id:30, label:'Tank Squadron (3 Demolishers)',		pts:200, upto:1},
+	{id:31, label:'Hellhound Squadron (3 Hellhounds)',	pts:150, upto:1},
+	{id:32, label:'Griffon Battery (3 Griffons)',		pts:100, upto:1},
+	{id:33, label:'2 Snipers',							pts:50, upto:1},
+	{id:34, label:'2 Ogryns',							pts:50, upto:1},
+	{id:35, label:'Hydra',								pts:50, upto:1}
+]};
+var chimera =			{id:26, label:'Chimera',	pts:25}; // only applies to snipers & ogryns
 var valkyrie =			{id:36, label:'Valkyrie Transport',				pts:150, upto:1};
 var emperor =			{id:37, label:'Emperor Class',					pts:150, upto:1};
 
 // SUPPORT (2 per company, no upgrades allowed)
-var roughRider =	{id:500, label:'Rough Rider Platoon', 			pts:150, upgrades:[], restricted:true, units:'6 Rough Riders'};
-var stormTrooper =	{id:501, label:'Storm Trooper Platoon',			pts:200, upgrades:[valkyrie], restricted:true, units:'8 Storm Troopers'};
-var basilisk =		{id:502, label:'Artillery Battery (Basilisk)',	pts:250, upgrades:[], restricted:true, units:'3 Basilisks'};
-var bombard =		{id:503, label:'Artillery Battery (Bombard)',	pts:250, upgrades:[], restricted:true, units:'3 Bombards'};
-var manticore =		{id:504, label:'Artillery Battery (Manticore)',	pts:250, upgrades:[], restricted:true, units:'3 Manticores'};
-var sentinel =		{id:505, label:'Sentinel Squadron',				pts:100, upgrades:[], restricted:true, units:'4 Sentinels'};
-var deathstrike =	{id:506, label:'Deathstrike Missile Battery',	pts:200, upgrades:[], restricted:true, upto:1, units:'2 Deathstrike Missile Launchers'};
-var baneblade =		{id:507, label:'Super-heavy Tank Platoon', 		pts:200, upgrades:[], restricted:true, defaults:[{unit:bane2, count:1}]};
-var orbital =		{id:508, label:'Orbital Support',				pts:150, upgrades:[], restricted:true, upto:1, defaults:[{unit:lunar,count:1}]};
-var flak =			{id:509, label:'Flak Battery',					pts:150, upgrades:[], restricted:true, units:'3 Hydra'};
-var vulture =		{id:510, label:'Vulture Squadron',				pts:300, upgrades:[], restricted:true, units:'4 Vultures'};
-var supportList =	{id:511, label:'SUPPORT FORMATIONS', options:[basilisk, bombard, manticore, deathstrike, flak, orbital, roughRider, sentinel, stormTrooper, baneblade, vulture]};
+var supportList =	{id:511, label:'SUPPORT FORMATIONS', options:[
+	{id:500, label:'Rough Rider Platoon', 			pts:150, upgrades:[], restricted:true, units:'6 Rough Riders'},
+	{id:501, label:'Storm Trooper Platoon',			pts:200, upgrades:[valkyrie], restricted:true, units:'8 Storm Troopers'},
+	{id:502, label:'Artillery Battery (Basilisk)',	pts:250, upgrades:[], restricted:true, units:'3 Basilisks'},
+	{id:503, label:'Artillery Battery (Bombard)',	pts:250, upgrades:[], restricted:true, units:'3 Bombards'},
+	{id:504, label:'Artillery Battery (Manticore)',	pts:250, upgrades:[], restricted:true, units:'3 Manticores'},
+	{id:505, label:'Sentinel Squadron',				pts:100, upgrades:[], restricted:true, units:'4 Sentinels'},
+	{id:506, label:'Deathstrike Missile Battery',	pts:200, upgrades:[], restricted:true, upto:1, units:'2 Deathstrike Missile Launchers'},
+	{id:507, label:'Super-heavy Tank Platoon', 		pts:200, upgrades:[superP], restricted:true},
+	{id:508, label:'Orbital Support',				pts:150, upgrades:[spacecraft], restricted:true, upto:1},
+	{id:509, label:'Flak Battery',					pts:150, upgrades:[], restricted:true, units:'3 Hydra'},
+	{id:510, label:'Vulture Squadron',				pts:300, upgrades:[], restricted:true, units:'4 Vultures'}
+]};
 
 // COMPANIES
-var hq = 			{id:512, label:'Regimental HQ', 				pts:500, upgrades:[chimera, companyUpgrades], restricting:true, upto:1, units:'Supreme Commander, 12 Infantry, 7 Chimera'};
-var infantryCoy = 	{id:513, label:'Infantry Company', 				pts:250, upgrades:[chimera, companyUpgrades], restricting:true, units:'Commander, 12 Infantry'};
-var mechCoy = 		{id:514, label:'Mechanised Infantry Company', 	pts:400, upgrades:[chimera, companyUpgrades], restricting:true, units:'Commander, 12 Infantry, 7 Chimera'};
-var tankCoy = 		{id:515, label:'Tank Company', 					pts:650, upgrades:[chimera, companyUpgrades], restricting:true, defaults:[{unit:russ, count:10}]};
-var superCoy = 		{id:516, label:'Super-heavy Tank Company', 		pts:500, upgrades:[chimera, companyUpgrades], restricting:true, defaults:[{unit:bane, count:3}]};
-var artilleryCoy = 	{id:517, label:'Artillery Company', 			pts:650, upgrades:[chimera, companyUpgrades], restricting:true, defaults:[{unit:basil, count:9}]};
-var companyList =	{id:518, label:'COMPANIES', options:[hq, infantryCoy, mechCoy, tankCoy, superCoy, artilleryCoy]};
+var companyList =	{id:518, label:'COMPANIES', options:[
+	{id:512, label:'Regimental HQ', 				pts:500, upgrades:[chimera, companyUpgrades], restricting:true, upto:1, units:'Supreme Commander, 12 Infantry, 7 Chimera'},
+ 	{id:513, label:'Infantry Company', 				pts:250, upgrades:[chimera, companyUpgrades], restricting:true, units:'Commander, 12 Infantry'},
+	{id:514, label:'Mechanised Infantry Company', 	pts:400, upgrades:[chimera, companyUpgrades], restricting:true, units:'Commander, 12 Infantry, 7 Chimera'},
+	{id:515, label:'Tank Company', 					pts:650, upgrades:[chimera, companyUpgrades, lemanRuss], restricting:true},
+	{id:516, label:'Super-heavy Tank Company', 		pts:500, upgrades:[chimera, companyUpgrades, superh], restricting:true},
+ 	{id:517, label:'Artillery Company', 			pts:650, upgrades:[chimera, companyUpgrades, artilleryOpts], restricting:true}
+]};
 
 // NAVY
-var thunderbolt = 		{id:519, label:'Thunderbolt Fighters',	pts:150, upgrades:[], limited:true, units:'2 Thunderbolts'};
-var marauder =			{id:520, label:'Marauder Bombers',		pts:300, upgrades:[], limited:true, units:'2 Marauders'};
-var navyList =			{id:521, label:'IMPERIAL NAVY', options:[thunderbolt, marauder]};
+var navyList =			{id:521, label:'IMPERIAL NAVY', options:[
+	{id:519, label:'Thunderbolt Fighters',	pts:150, upgrades:[], limited:true, units:'2 Thunderbolts'},
+	{id:520, label:'Marauder Bombers',		pts:300, upgrades:[], limited:true, units:'2 Marauders'}
+]};
 
 // TITANS
-var warlord =			{id:522, label:'Warlord Class Titan',	pts:850, upgrades:[], limited:true};
-var reaver =			{id:523, label:'Reaver Class Titan',	pts:650, upgrades:[], limited:true};
-var warhound =			{id:524, label:'Warhound Titan',		pts:250, upgrades:[], limited:true};
-var titanList = 		{id:525, label:'TITAN LEGIONS', options:[warlord, reaver, warhound]};
+var titanList = 		{id:525, label:'TITAN LEGIONS', options:[
+	{id:522, label:'Warlord Class Titan',	pts:850, upgrades:[], limited:true},
+	{id:523, label:'Reaver Class Titan',	pts:650, upgrades:[], limited:true},
+	{id:524, label:'Warhound Titan',		pts:250, upgrades:[], limited:true}
+]};
 
 // DETAILS
 var listData = {
--- war/SM_codex.js
+++ war/SM_codex.js
@@ -1,64 +1,68 @@
-// OPTIONALS
-var transport =			{id:10, label:'Transport', upto:1};
-	var rhinos =			{id:11, label:'Rhinos',		pts:0, optional:true, group:transport};
-	var pods =				{id:12, label:'Drop Pods',		pts:0, optional:true, group:transport};
-	transport.options =		[rhinos, pods];
-var bikes = 			{id:13, label:'Bikes',	upto:5, minimum:5};
-	var attackBike =		{id:14, label:'Attack Bike', 	pts:0, optional:true, group:bikes};
-	var bike =				{id:15, label:'Bike Unit',		pts:0, optional:true, group:bikes};
-	bikes.options = 		[attackBike, bike];
-var speeders =			{id:16, label:'Speeders', upto:5, minimum:5};
-	var speeder =			{id:17, label:'Land Speeder',					pts:0, group:speeders, optional:true};
-	var tornado =			{id:18, label:'Tornado',						pts:10, group:speeders, optional:true};
-	var typhoon = 			{id:19, label:'Typhoon',						pts:25, group:speeders, optional:true};
-	speeders.options =		[speeder, tornado, typhoon];
-var predators =			{id:20, label:'Predators', upto:4, minimum:4};
-	var predatorA =			{id:21, label:'Annihilator',		pts:0, group:predators, optional:true};
-	var predatorD =			{id:22, label:'Destructor',		pts:0, group:predators, optional:true};
-	predators.options = 	[predatorA, predatorD];
+// MANDATORY UNITS
+var transport =			{id:10, label:'Transport', upto:1, options:[
+	{id:11, label:'Rhinos',			pts:0, optional:true},
+	{id:12, label:'Drop Pods',		pts:0, optional:true}
+]};
+var bikes = 			{id:13, label:'Bikes',	upto:5, minimum:5, options:[
+	{id:15, label:'Bike Unit',		pts:0, optional:true},
+	{id:14, label:'Attack Bike', 	pts:0, optional:true}
+]};
+var speeders =			{id:16, label:'Speeders', upto:5, minimum:5, options:[
+	{id:17, label:'Land Speeder',	pts:0, optional:true},
+	{id:18, label:'Tornado',		pts:10, optional:true},
+	{id:19, label:'Typhoon',		pts:25, optional:true}
+]};
+var predators =			{id:20, label:'Predators', upto:4, minimum:4, options:[
+	{id:21, label:'Annihilator',	pts:0, optional:true},
+	{id:22, label:'Destructor',		pts:0, optional:true}
+]};
+
 // UPGRADES
-var commander =			{id:23, label:'Cmdr', upto:1};
-	var captain = 			{id:24, label:'Captain',			pts:50, group:commander};
-	var chaplain = 			{id:25, label:'Chaplain', 			pts:50, group:commander};
-	var librarian = 		{id:26, label:'Librarian', 		pts:50, group:commander};
-	var supreme = 			{id:27, label:'Supreme Commander',	pts:100, group:commander};
-	commander.options = 	[captain, chaplain, librarian, supreme];
-var dreadnought = 		{id:30, label:'Dreadnought', 						pts:50, upto:2};
-var hunter = 			{id:31, label:'Hunter', 							pts:75, upto:1};
-var landRaiders = 		{id:32, label:'Land Raider', 						pts:100, upto:4};
-var razorbacks =		{id:33, label:'Razorback',							pts:25};
-var sniper =			{id:34, label:'Sniper',							pts:25, upto:1};
-var vindicator = 		{id:35, label:'Vindicator',						pts:75, upto:2};
+var commander =			{id:23, label:'Cmdr', upto:1, options:[
+	{id:24, label:'Captain',			pts:50},
+	{id:25, label:'Chaplain', 			pts:50},
+	{id:26, label:'Librarian', 			pts:50},
+	{id:27, label:'Supreme Commander',	pts:100}
+]};
+var dreadnought = 		{id:30, label:'Dreadnought', 				pts:50, upto:2};
+var hunter = 			{id:31, label:'Hunter', 					pts:75, upto:1};
+var landRaiders = 		{id:32, label:'Land Raider', 				pts:100, upto:4};
+var razorbacks =		{id:33, label:'Razorback',					pts:25};
+var sniper =			{id:34, label:'Sniper',						pts:25, upto:1};
+var vindicator = 		{id:35, label:'Vindicator',					pts:75, upto:2};
 
 // DETACHMENTS
-var assaultD = 			{id:500, label:'Assault Detachment', 		pts:175, units:'4 Assault Units', upgrades:[commander, vindicator]};
-var bikeD = 			{id:501, label:'Bike Detachment', 			pts:200, defaults:[{unit:bike,count:5}], upgrades:[commander]};
-var devastatorD = 		{id:502, label:'Devastator Detachment',		pts:250, units:'4 Devastators', defaults:[{unit:rhinos,count:1}], upgrades:[commander, razorbacks, dreadnought, hunter, landRaiders, rhinos]};
-var landRaiderD = 		{id:503, label:'Land Raiders',				pts:400, units:'4 Land Raiders', upgrades:[commander, hunter, vindicator]};
-var landSpeederD =		{id:504, label:'Land Speeders',				pts:200, defaults:[{unit:speeder,count:5}], upgrades:[commander]};
-var landingCraftD =		{id:505, label:'Landing Craft',				pts:350, upgrades:[]};
-var predatorsD =		{id:506, label:'Predators',					pts:300, defaults:[{unit:predatorA,count:4}], upgrades:[commander, hunter, vindicator]};
-var scoutD =			{id:507, label:'Scout Detachment',			pts:150, units:'4 Scouts', defaults:[{unit:rhinos,count:1}], upgrades:[commander, razorbacks, sniper, rhinos]};
 var spacecraft = 		{id:508, label:'Spacecraft', upto:1};
-var strikeCruiserD =	{id:509, label:'Strike Cruiser',			pts:200, upgrades:[], group:spacecraft}; 
-var battleBargeD =		{id:510, label:'Battle Barge',				pts:350, upgrades:[], group:spacecraft}; 
-var tacticalD =			{id:511, label:'Tactical Detachment',		pts:300, units:'6 Tacticals', defaults:[{unit:rhinos,count:1}], upgrades:[commander, razorbacks, dreadnought, hunter, vindicator, rhinos]};
-var terminatorD =		{id:512, label:'Terminator Detachment',		pts:325, units:'4 Terminators', upgrades:[commander, vindicator, dreadnought, landRaiders]};
-var thunderhawkD =		{id:513, label:'Thunderhawk',				pts:200, upgrades:[]};
-var vindicatorD =		{id:514, label:'Vindicators',				pts:300, units:'4 Vindicators', upgrades:[commander, hunter]};
-var whirlwindD =		{id:515, label:'Whirlwinds',				pts:300, units:'4 Whirlwinds', upgrades:[commander, hunter]};
-var detachmentList =	{id:516, label:'DETACHMENTS', options:[assaultD, battleBargeD, bikeD, devastatorD, landRaiderD, landSpeederD, landingCraftD, predatorsD, scoutD, strikeCruiserD, tacticalD, terminatorD, thunderhawkD, vindicatorD, whirlwindD]};
+var detachmentList =	{id:516, label:'DETACHMENTS', options:[
+	{id:500, label:'Assault Detachment', 		pts:175, units:'4 Assault Units', upgrades:[commander, vindicator]},
+	{id:501, label:'Bike Detachment', 			pts:200, upgrades:[bikes, commander]},
+	{id:502, label:'Devastator Detachment',		pts:250, units:'4 Devastators', upgrades:[commander, razorbacks, dreadnought, hunter, landRaiders, transport]},
+	{id:503, label:'Land Raiders',				pts:400, units:'4 Land Raiders', upgrades:[commander, hunter, vindicator]},
+	{id:504, label:'Land Speeders',				pts:200, upgrades:[speeders, commander]},
+	{id:505, label:'Landing Craft',				pts:350, upgrades:[]},
+	{id:506, label:'Predators',					pts:300, upgrades:[predators, commander, hunter, vindicator]},
+	{id:507, label:'Scout Detachment',			pts:150, units:'4 Scouts', upgrades:[commander, razorbacks, sniper, transport]},
+	{id:509, label:'Strike Cruiser',			pts:200, upgrades:[], group:spacecraft}, 
+	{id:510, label:'Battle Barge',				pts:350, upgrades:[], group:spacecraft},
+	{id:511, label:'Tactical Detachment',		pts:300, units:'6 Tacticals', upgrades:[commander, razorbacks, dreadnought, hunter, vindicator, transport]},
+	{id:512, label:'Terminator Detachment',		pts:325, units:'4 Terminators', upgrades:[commander, vindicator, dreadnought, landRaiders]},
+	{id:513, label:'Thunderhawk',				pts:200, upgrades:[]},
+	{id:514, label:'Vindicators',				pts:300, units:'4 Vindicators', upgrades:[commander, hunter]},
+	{id:515, label:'Whirlwinds',				pts:300, units:'4 Whirlwinds', upgrades:[commander, hunter]}
+]};
 
 // NAVY
-var thunderbolt = 		{id:517, label:'Thunderbolt Fighters',	pts:150, units:'2 Fighters', upgrades:[], limited:true};
-var marauder =			{id:518, label:'Marauder Bombers',		pts:300, units:'2 Bombers', upgrades:[], limited:true};
-var navyList =			{id:519, label:'IMPERIAL NAVY', options:[thunderbolt, marauder]};
+var navyList =			{id:519, label:'IMPERIAL NAVY', options:[
+	{id:517, label:'Thunderbolt Fighters',	pts:150, units:'2 Fighters', upgrades:[], limited:true},
+	{id:518, label:'Marauder Bombers',		pts:300, units:'2 Bombers', upgrades:[], limited:true}
+]};
 
 // TITANS
-var warlord =			{id:520, label:'Warlord Class Titan',	pts:850, upgrades:[], limited:true};
-var reaver =			{id:521, label:'Reaver Class Titan',	pts:650, upgrades:[], limited:true};
-var warhound =			{id:522, label:'Warhound Titan',		pts:250, upgrades:[], limited:true};
-var titanList = 		{id:523, label:'TITAN LEGIONS', options:[warlord, reaver, warhound]};
+var titanList = 		{id:523, label:'TITAN LEGIONS', options:[
+	{id:520, label:'Warlord Class Titan',	pts:850, upgrades:[], limited:true},
+	{id:521, label:'Reaver Class Titan',	pts:650, upgrades:[], limited:true},
+	{id:522, label:'Warhound Titan',		pts:250, upgrades:[], limited:true}
+]};
 
 // DETAILS
 var listData = {
